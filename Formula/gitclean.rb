92d050c3cb2854695dab8ec7b8a15ec8d84c05a4
Drew Wyatt <drew.wyatt@onepeloton.com>
2019-04-12 18:16:50 -0500

    Fixed gitclean link

--- Formula/gitclean.rb
+++ Formula/gitclean.rb
@@ -1,7 +1,7 @@
 class Gitclean < Formula
   desc "Deletes ': gone' branches from local repositories"
   homepage "https://github.com/drewwyatt/gitclean"
-  url "https://github.com/drewwyatt/gitclean/releases/download/0.1.0/gitclean_0.1.0-next_Linux_x86_64.tar.gz"
+  url "https://github.com/drewwyatt/gitclean/releases/download/0.1.0/gitclean_0.1.0-next_Darwin_x86_64.tar.gz"
   version "0.1.0"
   sha256 "91038324e89fa320a24f41748e21499f134b816da4242182dcd1481d953e2975"
 
