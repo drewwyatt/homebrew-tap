2a03e92098e60f09e7dae9b8ee57ec2778391667
Drew Wyatt <drew.wyatt@onepeloton.com>
2019-04-12 18:03:39 -0500

    updated sha

--- Formula/gitclean.rb
+++ Formula/gitclean.rb
@@ -3,7 +3,7 @@ class Gitclean < Formula
   homepage "https://github.com/drewwyatt/gitclean"
   url "https://github.com/drewwyatt/gitclean/releases/download/0.1.0/gitclean_0.1.0-next_Linux_x86_64.tar.gz"
   version "0.1.0"
-  sha256 "fea465a64e42ed4a83004b7717f942397ea223a6726dc439bf31635f575a0eb4"
+  sha256 "91038324e89fa320a24f41748e21499f134b816da4242182dcd1481d953e2975"
 
   def install
     bin.install "gitclean"
