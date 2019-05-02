0a6afcdaa3e825e57c5a06e227b1c18ceb600ece
Drew Wyatt <drew.wyatt@onepeloton.com>
2019-05-02 12:29:19 -0400

    git-tidy 1.0.0

--- Formula/git-tidy.rb
+++ Formula/git-tidy.rb
@@ -1,11 +1,18 @@
 class GitTidy < Formula
-  desc "Delete local git branches that have a remote tracking branch that is ': gone'."
+  desc "Delete git branches that have a remote tracking branch that is : gone"
   homepage "https://github.com/drewwyatt/git-tidy"
-  url "https://github.com/drewwyatt/git-tidy/releases/download/0.4.0/git-tidy_0.4.0_Darwin_x86_64.tar.gz"
-  version "0.4.0"
-  sha256 "fcdf162cefbe755a69a0cc2744432c2d948555b0d8d754aefb7318277509ccab"
+  url "https://github.com/drewwyatt/git-tidy/archive/1.0.0.tar.gz"
+  sha256 "5bdae85d8ac4fe59305781846229017a8649d894f82ed9cba243ea1a534bd511"
+  depends_on "go" => :build
 
   def install
+    system "go", "build", "-ldflags", "-X main.version=#{version}"
     bin.install "git-tidy"
+  end
+
+  test do
+    output = `git tidy -v`
+    expected = "#{version}\n"
+    assert_equal expected, output
   end
 end
