5706f7e6cfafcef1d7f060054c08887dce88ea98
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-08-10 01:57:44 +0000

    git-tidy: add 2.0.1 bottle.

--- Formula/git-tidy.rb
+++ Formula/git-tidy.rb
@@ -5,6 +5,12 @@ class GitTidy < Formula
   sha256 "7f97331e08e66d5da0921a6a809d7b186bc9282f673295dd4aa122ae440429c1"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/drewwyatt/homebrew-tap/releases/download/git-tidy-2.0.1"
+    sha256 cellar: :any_skip_relocation, catalina:     "b58c04e4009dd66815458f94e7df2e5c6dc94694b1388e43307a8779139fb0fa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "893c065d698df7eeec4b909d9be39411dcee935db44cb31cc869563f24466b23"
+  end
+
   depends_on "rust" => :build
 
   def install
